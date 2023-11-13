4c8b8b86394ab0bf30ea88997982e43081415c17
brian-250 <brianmilian@csu.fullerton.edu>
2023-11-12 20:13:25 -0800

    Define newly added member functions

--- heart.cc
+++ heart.cc
@@ -1,3 +1,11 @@
 #include "heart.h"
 
 Heart::Heart() : heart_beat_(false) {}
+
+bool Heart::heartBeat() const {
+    return this->heart_beat_;
+}
+
+void Heart::setHeartBeat(bool heart_beat) {
+    this->heart_beat_ = heart_beat;
+}
