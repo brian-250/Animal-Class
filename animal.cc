767b8ac33b77e77fb60e596da495903ab40b9148
brian-250 <brianmilian@csu.fullerton.edu>
2023-11-15 15:10:10 -0800

    Define operator= member function

--- animal.cc
+++ animal.cc
@@ -38,3 +38,14 @@ void Animal::printAttributes() {
     std::cout << "My age is " << this->age_ << '\n';
     std::cout << "My color is " << this->color_ << '\n';
 }
+
+Animal& Animal::operator=(const Animal& other) {
+    // Check if pointer of this instance is not pointing to itself to avoid unecessary assignments
+    if (this != &other) {
+    // Assign member variable data from 'other' to 'this'
+    this->name_ = other.name_;
+    this->age_ = other.age_;
+    this->color_ = other.color_;
+    this->heart_->setHeartBeat(other.heart_->heartBeat());
+    }
+}
